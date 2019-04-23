a0a7f9a91b1c28471b74946ecbf7b7c129f83070
imsxz <lukelenhard@gmail.com>
2019-04-23 10:55:57 -0400

    bonus1

---
+++ bonus-1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bonus_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberGrade;
+            bool continuing = true;
+            string letterGrade;
+
+            while (continuing)
+            {
+                continuing = false;
+
+                Console.WriteLine("Enter a numerical grade: ");
+                numberGrade = int.Parse(Console.ReadLine());    
+
+                if (numberGrade >= 88)
+                {
+                    letterGrade = "A";
+                }
+                else if(numberGrade <= 87 && numberGrade >= 80)
+                {
+                    letterGrade = "B";
+                }
+                else if(numberGrade <= 79 && numberGrade >= 67)
+                {
+                    letterGrade = "C";
+                }
+                else if(numberGrade <= 66 && numberGrade >= 60)
+                {
+                    letterGrade = "D";
+                }
+                else
+                {
+                    letterGrade = "F";
+                }
+
+                Console.WriteLine("Letter grade: {0}.\nDo you want to continue?(Y/N)", letterGrade);
+
+                string answer = Console.ReadLine();
+                if (answer == "Y" || answer == "y")
+                {
+                    continuing = true;
+                }
+            }
+        }
+    }
+}
